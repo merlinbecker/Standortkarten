8d083e70446aa80d836bd951a362ba5e64835f32
merlinbecker <merlin.becker@googlemail.com>
2018-07-11 19:22:36 +0200

    neue sachen

---
+++ assets/js/marker.js
@@ -0,0 +1,91 @@
+function createMarkerOptions(markercolor,textcolor,art){
+	if(art=="")art="HW";
+	var options={};
+	options["circleText"]="HW";
+	options["className"]="svg-icon";
+	options["popupAnchor"]=[1, -34];
+	options["circleAnchor"]=null;
+	options["circleColor"]="#cccccc";
+	options["circleOpacity"]=null;
+	options["circleFillOpacity"]=1.0;
+    options["circleRatio"]=0.6;
+    options["circleWeight"]=1.0;
+	
+	options["fillOpacity"]=1.0;
+	options["fontColor"]=textcolor;
+	options["fontOpacity"]="1";
+	options["fontSize"]=null;
+	options["iconAnchor"]=null;
+	options["iconSize"]=L.point(32,48);
+	options["opacity"]=1;
+	
+	switch(art){
+		case "HW":
+			options["circleFillColor"]="#ffffff";
+			options["fontColor"]="black";
+			options["color"]="#000000";
+			options["fillColor"]=markercolor;
+			options["weight"]=2;
+		break;
+		case "ZW":
+			options["circleFillColor"]=markercolor;
+			options["color"]="#000000";
+			options["fillColor"]="#cccccc";
+			options["weight"]=2;
+		break;
+		case "HZW":
+			options["circleFillColor"]="#cccccc";
+			options["fontColor"]="black";
+			options["color"]="#000000";
+			options["fillColor"]=markercolor;
+			options["weight"]=2;
+		break;
+		case "GH":
+			options["circleFillColor"]="#ffffff";
+			options["fontColor"]="black";
+			options["color"]=markercolor;
+			options["fillColor"]="#cccccc";
+			options["weight"]=3;
+		break;
+	}
+	return options;
+}
+
+function getWerkTabelle(feature){
+	var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Name</th><td>" + feature.properties.Name1+" "+feature.properties.Name2+" "+feature.properties.Name3 + "</td></tr>";
+	  content+="<tr><th>Werk:</th><td>" ;
+		content+=getWerkArt(feature.properties.Art);
+		content+="</td></tr>";
+		content+="<tr><th>Tel:</th><td>" + feature.properties.Telefon + "</td></tr>";
+		content+="<tr><th>Fax:</th><td>" + feature.properties.Telefax + "</td></tr>";
+		content+="<tr><th>Adresse:</th><td>" + feature.properties.Strasse + "<br/>";
+		content+=feature.properties.PLZStrasse+" "+feature.properties.Ort+"</td></tr>";
+		content+="<tr><th>Postfach:</th><td>" + feature.properties.Postfach + "<br/>";
+		content+=feature.properties.PLZPostfach+"</td></tr>";
+		content+="<tr><th>E-Mail:</th><td><a href='mailto:" + feature.properties.Email + "'" + feature.properties.Email + "</td></tr>";
+		content+="<tr><th>Internet:</th><td><a class='url-break' href='http://"+ feature.properties.Internet +"' target='_blank'>"+ feature.properties.Internet+ "</td></tr>";
+		content+="</table>";
+	return content;
+}
+function getWerkArt(w){
+	switch(w){
+		case "":
+		case "HW":
+			return "Hauptwerk";
+		case "ZW":
+			return "Zweigwerk";
+		case "HZW":
+			return "Haupt-/Zweigwerk";
+		case "GH":
+			return "Gebietshauptwerk";
+			
+	}
+}
+
+function createMarker(markercolor,textcolor,feature,latlng){
+	var options=createMarkerOptions(markercolor,textcolor,feature.properties.Art);
+	options['circleText']=feature.properties.id;
+	
+	var marker=new L.Marker.SVGMarker(latlng,{iconOptions: options,riseOnHover:true}).bindTooltip(function (layer) {return '<b>'+getWerkArt(feature.properties.Art)+'</b> von:<br/>'+feature.properties.Name1+' '+feature.properties.Name2+' '+feature.properties.Name3;},{permanent: false, opacity: 0.9});
+	return marker;	
+}
