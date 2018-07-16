9f4f208d3d2ff013556c3bddf26ede4a48bb9fa0
merlinbecker <merlin.becker@googlemail.com>
2018-07-17 01:09:51 +0200

    Adminbereich1

--- verwaltung/index.php
+++ verwaltung/index.php
@@ -44,53 +44,24 @@ if(isset($_POST['command'])){
 				$ausgabe['branchen'][]=$db->antwort_reihe;
 			}while($db->neueReihe());
 		}
-		$anfrage="SELECT id,email,anrede,name,firma,anschrift FROM _sk_nutzer ORDER BY name ASC";
+		$anfrage="SELECT id,email,anrede,name,firma,anschrift FROM _sk_nutzer ORDER BY firma ASC";
 		$db->neueAnfrage($anfrage);
 		if($db->antwort_anzahl>0){
 		$abonnenten=array();
-			do{
-				$db->antwort_reihe['email']=utf8_encode($db->antwort_reihe['email']);
-				$db->antwort_reihe['anrede']=utf8_encode($db->antwort_reihe['anrede']);
-				$db->antwort_reihe['firma']=utf8_encode($db->antwort_reihe['firma']);
-				$db->antwort_reihe['anschrift']=utf8_encode($db->antwort_reihe['anschrift']);
-				
+			do{				
 				$tempdb=new DB_Verbindung("SELECT bundesland,branche FROM _sk_nutzer_bundesland_branche WHERE nutzerid=".$db->antwort_reihe['id']);
-				if($db->antwort_anzahl>0){
-					$db->antwort_reihe['buba_matrix']=$tempdb->antwort_reihe;
+				
+				if($tempdb->antwort_anzahl>0){
+					do{
+						$db->antwort_reihe['buba_matrix'][]=$tempdb->antwort_reihe;
+					}while($tempdb->neueReihe());
 				}
 				$ausgabe['abonnenten'][]=$db->antwort_reihe;
 			}while($db->neueReihe());
 		}
 		echo json_encode($ausgabe);
 		break;
-	}
-
-}
-else include("verwaltung.html");
-exit();
-
-
-
-
-/**POST Aktionen abfragen**/
-if(isset($_POST['action'])){
-	switch($_POST['action']){
-		case "changeSettings":
-			//zunächst alle Zuordnungen löschen
-			$db->neueAnfrage("DELETE FROM _sk_nutzer_bundesland_branche WHERE nutzerid=".$_POST['uid']);
-			
-			//dann wieder einfügen
-			foreach($_POST['bubra'] as $bundesland => $branchen){
-				foreach($branchen as $branche=>$val){
-					$db->neueAnfrage("INSERT INTO _sk_nutzer_bundesland_branche (nutzerid,bundesland,branche) VALUES (".$_POST['uid'].",".$bundesland.",".$branche.")");
-				}
-			}
-		break;
-		case "delete":
-			$db->neueAnfrage("DELETE FROM _sk_nutzer_bundesland_branche WHERE nutzerid=".$_POST['uid']);
-			$db->neueAnfrage("DELETE FROM _sk_nutzer WHERE id=".$_POST['uid']);
-		break;
-		case "createUser":
+	case "createUser":
 			if(isset($_POST['email'])){
 				//generiere einen Aktivierungslink
 				$activation=md5(time());
@@ -120,190 +91,40 @@ if(isset($_POST['action'])){
 				$nachricht.="<p><img border=\"0\" src=\"http://standortkarten.geoplangmbh.de/daten/bilder/logo_stein.jpg\"></p>";
 				$nachricht.="</body></html>";
 				
-// für HTML-E-Mails muss der 'Content-type'-Header gesetzt werden
-$header  = 'MIME-Version: 1.0' . "\r\n";
-$header .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+				// für HTML-E-Mails muss der 'Content-type'-Header gesetzt werden
+				$header  = 'MIME-Version: 1.0' . "\r\n";
+				$header .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 
-// zusätzliche Header
-$header .= "To: ".$_POST['anrede']." ".$_POST['name']."<".$_POST['email'].">" . "\r\n";
-$header .= 'From: Standortkarten Aktivierung <mail@stein-verlag.de>' . "\r\n";
+				// zusätzliche Header
+				$header .= "To: ".$_POST['anrede']." ".$_POST['name']."<".$_POST['email'].">" . "\r\n";
+				$header .= 'From: Standortkarten Aktivierung <mail@stein-verlag.de>' . "\r\n";
 
-// verschicke die E-Mail
-mail($empfaenger, $betreff, $nachricht, $header);
-				
+				// verschicke die E-Mail
+				mail($empfaenger, $betreff, $nachricht, $header);
+			echo "success!";
 			}
 		break;
-	}
-}
-function utf8_encode_deep(&$input) {
-	if (is_string($input)) {
-		$input = utf8_encode($input);
-	} else if (is_array($input)) {
-		foreach ($input as &$value) {
-			utf8_encode_deep($value);
-		}
-		
-		unset($value);
-	} else if (is_object($input)) {
-		$vars = array_keys(get_object_vars($input));
-		
-		foreach ($vars as $var) {
-			utf8_encode_deep($input->$var);
-		}
-	}
-}
-
-
-function getAboTabelle($uid=0){
-$output="";
-//Schleife durch alle Bundesländer laufen und durch alle Branchen
-//Zeilen: Bundesländer
-//Spalten: Branchen
-$output.="<table class\"detailtabelle\">";
-$tempdb=new DB_Verbindung("SELECT * FROM _sk_branche ORDER BY beschreibung ASC");
-$tempdb2=new DB_Verbindung("SELECT 1");
-$branchen=array();
-	//Tabellenheader erzeugen
-	$output.="<thead><tr>";
-	//erste Spalte freilassen
-	$output.="<th></th>";
-	if($tempdb->antwort_anzahl>0){
-		do{
-			$branchen[]=$tempdb->antwort_reihe['id'];
-			$output.="<th>".utf8_encode($tempdb->antwort_reihe['beschreibung'])."</th>";
-		}while($tempdb->neueReihe());
-	$output.="</tr></thead>";
-	
-		//jetzt die Bundesländer holen und anzeigen
-		$output.="<tbody>";
-			$tempdb->neueAnfrage("SELECT * FROM _sk_bundesland ORDER BY beschreibung ASC");
-			if($tempdb->antwort_anzahl>0){
-				do{
-					$output.="<tr><td>".utf8_encode($tempdb->antwort_reihe['beschreibung'])."</td>";
-					for($i=0;$i<count($branchen);$i++){
-						//TODO: prüfen, ob schon gecheckt!!
-						$tempdb2->neueAnfrage("SELECT nutzerid FROM _sk_nutzer_bundesland_branche WHERE bundesland=".$tempdb->antwort_reihe['id']." AND branche=".$branchen[$i]." AND nutzerid=".$uid);
-						if($tempdb2->antwort_anzahl>0)$checked="checked=\"checked\"";
-						else $checked="";
-						$output.="<td align=\"center\"><input type=\"checkbox\" name=\"bubra[".$tempdb->antwort_reihe['id']."][".$branchen[$i]."]\" ".$checked."></td>";
-					}
-					$output.="</tr>";
-				}while($tempdb->neueReihe());
+		case "delete":
+			$db->neueAnfrage("DELETE FROM _sk_nutzer_bundesland_branche WHERE nutzerid=".$_POST['uid']);
+			$db->neueAnfrage("DELETE FROM _sk_nutzer WHERE id=".$_POST['uid']);
+			echo "success!";
+		break;
+		case "changeSettings":
+			//zunächst alle Zuordnungen löschen
+			$db->neueAnfrage("DELETE FROM _sk_nutzer_bundesland_branche WHERE nutzerid=".$_POST['uid']);
+			
+			//dann wieder einfügen
+			foreach($_POST['bubra'] as $bundesland => $branchen){
+				foreach($branchen as $branche=>$val){
+					$db->neueAnfrage("INSERT INTO _sk_nutzer_bundesland_branche (nutzerid,bundesland,branche) VALUES (".$_POST['uid'].",".$bundesland.",".$branche.")");
+				}
 			}
-		$output.="</tbody>";
-	
+			
+			echo "success!";
+		break;
 	}
-$output.="</table>";
-return $output;
+
 }
-
-?>
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-<script type="text/javascript" src="../js/jq.js"></script>	
-<script type="text/javascript">
-	$(document).ready(function(evt){
-		$("form[name='delete']").submit(function(evt){
-			if(confirm("Möchten Sie diesen Benutzer wirklich löschen?")){
-				return true;
-			}
-			else return false;
-		});
-		$("form[name='createUser']").submit(function(evt){
-			if(confirm("Möchten Sie diesen Benutzer anlegen? Er bekommt einen Aktivierungslink an die angegebene E-Mail Adresse gesendet")){
-				return true;
-			}
-			else return false;
-		});
-	});
-</script>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Standortkarten Verwaltungsbereich</title>
-</head>
-<body>
-<table align="center" border=1>
-	<tr>
-		<td>
-		<form name="filter" method="POST" action="">
-		<input type="text" name="filtertext" id="filtertext" placeholder="Nach Namen suchen..." value="<?=$_POST['filtertext']?>">
-		<?php
-			if(isset($_POST['filtertext']))echo "<input form=\"filter\" type=\"reset\" onclick=\"document.getElementById('filtertext').value=''\" value=\"Suche zurücksetzen\">";
-			echo "<input type=\"submit\" value=\"Suche starten\">";
-		?>
-		</form>
-		</td>
-	</tr>
-</table><br/><br/>
-
-<table align="center" border=1>
-<thead><tr>
-<th>#</th>
-<th>Email</th>
-<th>Name</th>
-<th>Firma</th>
-<th>Info</th>
-<th><form name="newuser" method="POST" action=""><input type="hidden" name="action" value="formnewuser"/><input type="submit" value="neuer Benutzer"/></form></th>
-<tr>
-</thead>
-<tbody>
-<?php
-if(isset($_POST['filtertext'])){
-	$anfrage="SELECT * FROM _sk_nutzer WHERE name LIKE '%".mysql_real_escape_string($_POST['filtertext'])."%' ORDER BY name ASC";
-}
-else
-$anfrage="SELECT * FROM _sk_nutzer ORDER BY name ASC";
-$db->neueAnfrage($anfrage);
-//wenn es sich um einen neuen Nutzer handelt
-$output="";
-if($_POST['action']=="formnewuser"){
-	$output.="<form name=\"createUser\" method=\"POST\"><input type=\"hidden\" name=\"action\" value=\"createUser\"/>";
-	$output.="<tr>";
-	$output.="<td></td>";
-	$output.="<td><input type=\"text\" name=\"email\" placeholder=\"E-Mail Adresse\" /></td>";
-	$output.="<td><select name=\"anrede\"><option value=\"Herr\">Herr</option><option value=\"Frau\">Frau</option></select><input type=\"text\" name=\"name\" placeholder=\"Name\" /></td>";
-	$output.="<td><input type=\"text\" name=\"firma\" placeholder=\"Firma\" /></td>";
-	$output.="<td></td>";
-	$output.="<td><input type=\"submit\" value=\"Nutzer anlegen\" /></td>";
-	$output.="</tr>";
-	$output.="</form>";
-}
-
-if($db->antwort_anzahl>0){
-
-	do{
-		$output.="<tr>";
-		$output.="<td>".$db->antwort_reihe['id']."</td>";
-		$output.="<td>".$db->antwort_reihe['email']."</td>";
-		$output.="<td>".$db->antwort_reihe['vorname']." ".$db->antwort_reihe['name']."</td>";
-		$output.="<td>".$db->antwort_reihe['firma']."</td>";
-		$output.="<td><form name=\"details\" method=\"POST\"><input type=\"hidden\" name=\"uid\" value=\"".$db->antwort_reihe['id']."\"/><input type=\"hidden\" name=\"action\" value=\"showDetails\" /><input type=\"submit\" value=\"Abo anzeigen\"></form></td>";
-		$output.="<td><form name=\"delete\" method=\"POST\" action=\"\"><input type=\"hidden\" name=\"action\" value=\"delete\"/><input type=\"hidden\" name=\"uid\" value=\"".$db->antwort_reihe['id']."\"/><input type=\"submit\" value=\"Nutzer löschen\"></form></td>";
-		$output.="</tr>";
-		//wenn für diesen Nutzer Details angezeigt werden sollen
-		if($_POST['action']=='showDetails'&&$_POST['uid']==$db->antwort_reihe['id']){
-			$output.="<tr class=\"details\">";
-			$output.="<td colspan=\"6\"><h3>gebuchte Branchen des Nutzers ".$db->antwort_reihe['vorname']." ".$db->antwort_reihe['name'].":</h3>";
-			$output.="<form name=\"changeSettings\" method=\"POST\">";
-			$output.="<input type=\"hidden\" name=\"action\" value=\"changeSettings\"/>";
-			$output.="<input type=\"hidden\" name=\"uid\" value=\"".$db->antwort_reihe['id']."\"/>";
-			$output.=getAboTabelle($db->antwort_reihe['id']);
-			$output.="<input type=\"submit\" value=\"Buchung speichern\" />";
-			$output.="</form>";
-			$output.="</td>";
-			$output.="</tr>";
-		}
-	}while($db->neueReihe());
-
-}	
-echo $output;
-
-
+else include("verwaltung.html");
 unset($_POST);
-
-?>
-</tbody>
-</table>
-</body>
-</html>
+exit();
--- verwaltung/verwaltung.js
+++ verwaltung/verwaltung.js
@@ -1,9 +1,16 @@
+var abonnenten;
+var options = {
+    valueNames: [ 'name', 'city' ]
+};
+
 $(document).ready(function(evt){
-	$("#abo").hide();
+	$("#abo").show();
 	$("#standort").hide();
 	$("#printwerk").hide();
 	
 	$(".nav-link").click(function(evt){
+		if($(this).hasClass("disabled")) return false;
+		
 		$(".nav-link").removeClass("active");
 		$(this).addClass("active");
 		$(".container").hide();
@@ -12,7 +19,135 @@ $(document).ready(function(evt){
 		evt.stopPropagation();
 		return false;
 	});
-	$.post( "#","command=fetchAboData",function( data ) {
-	  $( ".result" ).html( data );
+	
+	$("#newAboModalForm").submit(function(evt){
+		$.post( "#","command=createUser&"+$(this).serialize(),function(data){
+			if(data=="success!"){
+				$("#newAboModalForm")[0].reset();
+				$("#newAboModal").modal('hide');
+				holeAbonnenten();
+			}
+		});
+		evt.stopPropagation();
+		return false;
 	});
+	
+	$("#aboSettingsModalForm").submit(function(evt){
+		$.post( "#","command=changeSettings&"+$(this).serialize(),function(data){
+			if(data=="success!"){
+				$("#aboSettingsModalForm")[0].reset();
+				$("#aboSettingsModal").modal('hide');
+				holeAbonnenten();
+			}
+		});
+		evt.stopPropagation();
+		return false;
+	});
+	
+	
+	
+	holeAbonnenten();
+	
+	
 });
+
+function createAboTable(branchen,bundeslaender,nutzermatrix){
+	nutzermatrix=nutzermatrix.abo;
+	var output="<table  class=\"table table-sm\">"
+	output+="<thead><tr>";
+	output+="<th scope=\"col\"></th>";
+	
+	$.each(branchen,function(index,value){
+		output+="<th scope=\"col\">"+value.beschreibung+"</th>";
+	});
+	output+="</tr></thead>";
+	output+="<tbody>";
+	
+	$.each(bundeslaender,function(index,value){
+		output+="<tr><th scope=\"row\">"+value.beschreibung+"</th>";
+		$.each(branchen,function(index2,value2){
+			//todo: find out if checked!
+			var checked="";
+			$.each(nutzermatrix,function(index,value3){
+				if(value3.bundesland==value.id&&value3.branche==value2.id){
+					checked="checked=\"checked\"";
+				}
+			});
+			output+="<td align=\"center\"><input type=\"checkbox\" name=\"bubra["+value.id+"]["+value2.id+"]\" "+checked+"></td>";
+		});
+		output+="</tr>";
+	});
+	output+="</tbody>";
+	output+="</table>";
+
+	return output;
+}
+
+
+function holeAbonnenten(){
+	$.post( "#","command=fetchAboData",function(data) {
+		abonnenten=JSON.parse(data);
+		
+		
+		$("#abo").empty();
+		$("#abo").append($("<form class=\"form-inline\"><input type=\"search\" class=\"form-control mb-2 mr-sm-2 mb-sm-0 search\" id=\"inlineFormInput\" placeholder=\"Suchen...\"><button type=\"button\" class=\"btn btn-primary sort\" data-sort=\"abo_firma\"><i class=\"fa fa-sort\"></i>&nbsp;Firma</button>&nbsp;&nbsp;<button type=\"button\" class=\"btn btn-primary sort\" data-sort=\"abo_name\"><i class=\"fa fa-sort\"></i>&nbsp;Ansprechpartner</button>&nbsp;&nbsp;<button type=\"button\" id=\"abo_new\" class=\"btn btn-success\"><i class=\"fa fa-plus-square\"></i>&nbsp;neuer Abonnent</button></form><ul class=\"list-group list\" id=\"abo_list\">"));
+		
+		$.each(abonnenten.abonnenten,function(ind,value){
+			/*
+			Todo: durch Handlebars.js ersetzen
+			*/
+			var output="";
+			output+="<li class=\"list-group-item\">";
+			output+="<h5 class=\"abo_firma\">"+value.firma+"</h5>";
+			if(value.name!=""){
+				output+=value.anrede+" "+"<span class=\"abo_name\">"+value.name+"</span> | ";
+			}
+			output+="<a class=\"abo_email\" href=\"mailto:"+value.email+"\" target=\"_blank\">"+value.email+"</a>";
+			
+			if(value.anschrift!=""){
+				output+="<span class=\"abo_anschrift\">"+value.anschrift+"</span>";
+			}
+			output+="<div class=\"collapse\" id=\"abomenu_"+value.id+"\">";
+			output+="<br/><button type=\"button\" class=\"abochange btn btn-primary\" data-ref=\""+value.id+"\"><i class=\"fa fa-th\"></i>&nbsp;Abos verwalten</button>&nbsp;";
+			output+="<button type=\"button\" class=\"abodelete btn btn-danger\" data-ref=\""+value.id+"\"><i class=\"fa fa-trash\"></i>&nbsp;Entfernen</button>";	
+			output+="</div>";
+		
+			output+="</li>";
+			
+			var el=$(output);
+			el.click(function(evt){
+				$("#abomenu_"+value.id).collapse();
+			});
+			$("#abo_list").append(el);
+			$("#abomenu_"+value.id).data("abo",value.buba_matrix);
+		});
+	
+		var options = {
+		  valueNames:['abo_name','abo_firma','abo_email','abo_anschrift']
+		};
+		var hackerList = new List('abo', options);
+		hackerList.sort("abo_firma", {order:"asc"});
+		
+		$("#abo_new").click(function(evt){
+			$("#newAboModal").modal();
+		});
+		
+		$(".abodelete").click(function(evt){
+			if(confirm("Möchten Sie den Abonnenten wirklich löschen?")){
+				$.post( "#","command=delete&uid="+$(this).attr("data-ref"),function(data){
+				if(data=="success!"){
+					holeAbonnenten();
+				}
+				});
+			}
+		});
+		
+		$(".abochange").click(function(evt){
+			$("#aboSettingsModalMatrix").empty();
+			$("#abochange_uid").val($(this).attr("data-ref"));
+			$("#aboSettingsModalMatrix").append(createAboTable(abonnenten.branchen,abonnenten.bundeslaender,$("#abomenu_"+$(this).attr("data-ref")).data()));
+			$("#aboSettingsModal").modal();
+		});
+		
+	});
+}
